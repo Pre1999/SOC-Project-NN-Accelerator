4b635d87e2ad2eb96df216f42bf44c11f014c6f2
695 ECE <695r11@ecegrid-thin5.ecn.purdue.edu>
2021-12-05 19:16:35 -0500

    adder mul

---
+++ SOC_Project_NN/software/NN/obj/default/mlp.d
@@ -0,0 +1,37 @@
+obj/default/mlp.o: mlp.c kann.h kautodiff.h kann_data.h \
+ ../NN_bsp//HAL/inc/io.h ../NN_bsp//HAL/inc/alt_types.h \
+ ../NN_bsp//drivers/inc/altera_avalon_performance_counter.h \
+ ../NN_bsp//HAL/inc/alt_types.h \
+ ../NN_bsp//drivers/inc/altera_up_avalon_video_pixel_buffer_dma.h \
+ ../NN_bsp//HAL/inc/sys/alt_dev.h ../NN_bsp/system.h ../NN_bsp/linker.h \
+ ../NN_bsp//HAL/inc/sys/alt_llist.h \
+ ../NN_bsp//HAL/inc/priv/alt_dev_llist.h \
+ ../NN_bsp//drivers/inc/altera_up_avalon_video_character_buffer_with_dma.h
+
+kann.h:
+
+kautodiff.h:
+
+kann_data.h:
+
+../NN_bsp//HAL/inc/io.h:
+
+../NN_bsp//HAL/inc/alt_types.h:
+
+../NN_bsp//drivers/inc/altera_avalon_performance_counter.h:
+
+../NN_bsp//HAL/inc/alt_types.h:
+
+../NN_bsp//drivers/inc/altera_up_avalon_video_pixel_buffer_dma.h:
+
+../NN_bsp//HAL/inc/sys/alt_dev.h:
+
+../NN_bsp/system.h:
+
+../NN_bsp/linker.h:
+
+../NN_bsp//HAL/inc/sys/alt_llist.h:
+
+../NN_bsp//HAL/inc/priv/alt_dev_llist.h:
+
+../NN_bsp//drivers/inc/altera_up_avalon_video_character_buffer_with_dma.h:
